db3b9eea221ce6374d0a9e39aea9b553b30b3bf4
maykel <maykel.winchester@hotmail.com>
2024-06-16 19:52:47 -0300

    feat: add characters screen

---
+++ src/presentation/screens/CharactersScreen/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import styled from 'styled-components/native';
+
+export default function CharactersScreen() {
+  return (
+    <Container>
+      <Text>CharactersScreen</Text>
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  flex: 1;
+`;
